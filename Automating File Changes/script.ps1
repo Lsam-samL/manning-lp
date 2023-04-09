1c2a2e9a29dfe643dedcb9eb9e0393f66ef3da3b
Lsam-samL <b.dekker@stokvis.eu>
2023-04-09 12:04:41 +0200

    My script to change je jpg files

---
+++ Automating File Changes/script.ps1
@@ -0,0 +1,18 @@
+#for this sctipt to work your working directory should
+#be the directory which has the Images directory as subdirectory
+function Rename_If_Image{
+    param ( 
+        [string]$ImageName
+    )
+    if ($ImageName -match ".jpg$")
+    {
+        $ImageName -match "\d{6}"
+        $NewName = $matches[0] + ".jpg"
+        Rename-Item -Path $ImageName -NewName $NewName  
+    }
+     
+
+}
+Get-ChildItem .\images | ForEach-Object {Rename_If_Image($_)} | Out-Null
+"The jpg-files have been renamed"
+
